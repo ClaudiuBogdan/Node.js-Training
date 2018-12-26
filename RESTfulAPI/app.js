a37d03de3480ebf8f572e7add377abd8552f96ed
Claudiu Constantin Bogdan <bogdan.claudiu93@gmail.com>
2018-12-27 00:05:17 +0200

    Added PUT request to change tool name.

--- RESTfulAPI/app.js
+++ RESTfulAPI/app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const Joi = require('joi');
+
 const app = express();
 
 app.use(express.json());
@@ -28,11 +30,23 @@ app.get('/api/tools', (req, res) => {
 
 app.get('/api/tools/:id', (req, res) => {
     const tool = toolsAvailable.find( c => c.id === parseInt(req.params.id));
-    if(!tool) res.status(404).send('Resource not found');
+    if(!tool) {
+        res.status(404).send('Resource not found');
+        return;
+    }
     res.send(tool);
 });
 
 app.post('/api/tools', (req, res) => {
+    //Code validation
+
+    const {error} = validateTool(req.body);
+    if(error){
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+    
+    //Item creation
     const tool = {
         id: toolsAvailable.length + 1,
         name: req.body.name
@@ -40,6 +54,30 @@ app.post('/api/tools', (req, res) => {
     toolsAvailable.push(tool);
     res.send(tool);
 });
+
+app.put('/api/tools/:id', (req, res) => {
+    const tool = toolsAvailable.find( c => c.id === parseInt(req.params.id));
+    if(!tool) {
+        res.status(404).send('Resource not found');
+        return;
+    }
+
+    const {error} = validateTool(req.body);
+    if(error){
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+    
+    tool.name = req.body.name;
+    res.send(tool);
+});
+
+const validateTool = (tool) => {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+    return Joi.validate(tool, schema);
+};
 
 const port = process.env.PORT || 8090;
 
